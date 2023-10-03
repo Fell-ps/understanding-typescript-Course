c15f4e3250e453baee4809f41aaef2684f992516
Fellps <felps369369@gmail.com>
2023-10-03 09:35:43 -0300

    "Understanding Compiler"

--- app.ts
+++ app.ts
@@ -1,16 +1,32 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
+type Combinable = number | string;
+type ConversionDescriptor = "as-number" | "as-text";
+
+function combine(
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
+) {
+  let result;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
   } else {
-    return result;
+    result = input1.toString() + input2.toString();
   }
+  return result;
+  /*if (resultConversion === 'as-number') {
+    return +result;
+  } else {
+  return result.toString();
+  }*/
 }
+const combinedAges = combine(30, 26, "as-number");
+console.log(combinedAges);
 
-let number1: number;
-number1 = 2;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = "Result is: ";
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
 
-add(number1, number2, printResult, resultPhrase);
+const combinedNames = combine("Max", "Anna", "as-text");
+console.log(combinedNames);
